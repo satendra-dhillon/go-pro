d0dadfd1bd17e04a407f8118831b73ed5061a426
satendra singh <satendra@meter.taxi>
2019-03-13 16:45:07 +0530

    added card game functionlity

---
+++ deck_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 20 {
+		t.Errorf("Expected deck length of 20, but got %v", len(d))
+	}
+
+	if d[0] != "Aceof Spades" {
+		t.Errorf("Expected first card of the list id Aceof Spaces, but getting %v", d[0])
+	}
+
+	if d[len(d)-1] != "Fiveof Clubs" {
+		t.Errorf("Expected last card of the list id Fiveof Clubs, but getting %v", d[0])
+	}
+}
+
+func TestSaveToDeckandNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 20 {
+		t.Errorf("Expected 20 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+	// cards := newDeck()
+	// hand, remainingCards := deal(cards, 5)
+
+	// hand.print()
+	// remainingCards.print()
+
+	// cards := newDeck()
+	// cards.saveToFile("my_cards")
+
+	// cards := newDeckFromFile("my_cards")
+	// cards.print()
+
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
